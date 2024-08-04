d0b6b7b0ec4ad2e5c4a21301d797d4b08c2fe63b
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2024-08-04 23:29:21 +0300

    feat: 965 of 999 solved part of work done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function toReadable (number) {
-  
+   const numbers = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+  if (number < 10){
+      return numbers[number];
+  }
+  if (number < 19){
+      switch (number){
+          case 10:
+              return "ten";
+          case 11:
+              return "eleven";
+          case 12:
+              return "twelve";
+          case 13:
+              return "thirteen";
+          case 14:
+              return "fourteen";
+          case 15:
+              return "fifteen";
+          case 16:
+              return "sixteen";
+          case 17:
+              return "seventeen";
+          case 18:
+              return "twenty";
+          case 19:
+              return "nineteen";
+      }
+  }
+  if (number >19 && number < 30){
+
+      return 'twenty' + ' ' + numbers[number%10];
+  }
+  if (number > 100){
+      let numberOfHundreds = Math.floor(number/100);
+      return numbers[numberOfHundreds] + ' ' +'hundred';
+  }
+
 }
